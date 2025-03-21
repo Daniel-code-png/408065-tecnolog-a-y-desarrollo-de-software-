a9482f5ce438fc2755c63efab9f5ed7dbff79b8b
Daniel-code-png <daniel.soto01@unicatolica.edu.co>
2025-03-20 23:57:53 -0500

    tarea

---
+++ Trabajo/resta.py
@@ -0,0 +1,11 @@
+a=2
+b=6
+c=0
+    
+while a != b:
+    a -=1
+    c +=1
+
+    
+
+print("La resta es: ", c)
---
+++ Trabajo/suma.py
@@ -0,0 +1,11 @@
+a=2
+b=3
+c=b
+while a>0:
+    c +=1
+    a-=1
+
+    
+    
+
+print("La suma es: ", c)
